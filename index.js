ba7e343e9afa1e9227a6361dce08210a2e997d6a
Randhir Kumar <veerrandhir3@gmail.com>
2026-02-01 14:20:33 +0530

    fix: correct spelling errors in server listening log and update dotenv path

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+import connectDB from "../db";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB() //if db connect then fire call back to listen express fn
+  .then(() => {
+    app.lieten(process.env.PORT || 8000, () => {
+      console.log(
+        `@ Connected server is listining at port: ${process.env.PORT}`,
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection Failed !!! ", err);
+  });
