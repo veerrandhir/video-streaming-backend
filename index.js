a42d3951f2bc97fc05a141a5122567c52d3ee19a
Randhir Kumar <veerrandhir3@gmail.com>
2026-02-01 14:25:27 +0530

    fix: correct typo in app.listen method in index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import dotenv from "dotenv";
 
 import connectDB from "../db";
 
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./.env",
 });
 
 connectDB() //if db connect then fire call back to listen express fn
   .then(() => {
-    app.lieten(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(
         `@ Connected server is listining at port: ${process.env.PORT}`,
       );
