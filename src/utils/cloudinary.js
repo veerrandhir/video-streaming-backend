780f36991ce61745e5da0d01af2b818542735b51
Randhir Kumar <veerrandhir3@gmail.com>
2026-02-01 22:02:29 +0530

    fix: add missing comment for local file deletion after successful upload in uploadOnCloudinary function

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,6 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
     // file has been uploaded successfully!!
     console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath); // once file upload to db delete file form local storage
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
